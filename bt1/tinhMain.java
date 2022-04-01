22727cc3fb52d675b8fcbdaaf2ea21bf66867905
QuocHuyTruong <minhhuytruong10@gmail.com>
2022-04-01 07:44:32 +0700

    push code 2

--- bt1/tinhMain.java
+++ bt1/tinhMain.java
@@ -20,5 +20,7 @@ public class tinhMain {
         System.out.println("75 + 12 = "+ct.tinh(75, 12));
         ct.setTinhToan(new Tru());
         System.out.println("54 - 78 = "+ct.tinh(54, 78));
+        ct.setTinhToan(new Chia());
+        System.out.println("54 / 78 = "+ct.tinh(54, 78));
     }
 }
--- election_singleton/bt4_SingletonMain.java
+++ election_singleton/bt4_SingletonMain.java
@@ -2,26 +2,23 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package stratery.duck_ex;
+package election_singleton;
 
 /**
  *
  * @author DELL
  */
-public class DuckExMain {
+public class bt4_SingletonMain {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Duck duck = new Vitxiem();
-//        IFlybehavior flybehavior=new Flynoway();
-        IQuackbehavior quackbehavior=new Squeak();
-        IFlybehavior thembay = new themBay();
-        duck.setFly(thembay);
-        duck.setQuack(quackbehavior);
-        duck.display();
+        User s1 = new User("huy");
+        s1.Vote(Canadidate.Donaltrum);
+        System.out.println("thang trum"+ Election.getIntance().getDol());
+        System.out.println("thang joe"+ Election.getIntance().getJoe());
     }
     
 }
